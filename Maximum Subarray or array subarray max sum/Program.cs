2be725dad10c0b48791dae94bfee2e6c3bad2555
joydeeprony89 <joydeeprony89@gmail.com>
2022-06-22 09:26:23 +0530

    Update Program.cs

--- Maximum Subarray or array subarray max sum/Program.cs
+++ Maximum Subarray or array subarray max sum/Program.cs
@@ -10,8 +10,6 @@ namespace Maximum_Subarray_or_array_subarray_max_sum
             System.Console.WriteLine(p.MaxSubArray(nums));
         }
 
-        public int MaxSubArray(int[] nums)
-        {
             public class Solution {
                 public int MaxSubArray(int[] nums) {
                   // dp array will have the possible max subarray sum at that current index, as the one element itself is a subarray of an array so at any position max subarray sum = Math.Max(nums[i], nums[i]+ dp[i -1]), current position item if is still maximum of subarraySum of nums[i]+dp[i-1](previous max which we have already calculated) 
@@ -27,6 +25,5 @@ namespace Maximum_Subarray_or_array_subarray_max_sum
                   return maxSum;
                 }
             }
-        }
     }
 }
