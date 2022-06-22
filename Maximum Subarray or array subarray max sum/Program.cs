cfead5bbbb4bebf0d5ad0b4dfe37c67dababae60
joydeeprony89 <joydeeprony89@gmail.com>
2022-06-22 09:25:26 +0530

    Update Program.cs
    updated the code with more readable approach and used DP

--- Maximum Subarray or array subarray max sum/Program.cs
+++ Maximum Subarray or array subarray max sum/Program.cs
@@ -12,23 +12,21 @@ namespace Maximum_Subarray_or_array_subarray_max_sum
 
         public int MaxSubArray(int[] nums)
         {
-            int length = nums.Length;
-            int currentMax = nums[0];
-            int globalMax = nums[0];
-            for (int i = 1; i < length; ++i)
-            {
-                if (currentMax < 0)
-                {
-                    currentMax = nums[i];
+            public class Solution {
+                public int MaxSubArray(int[] nums) {
+                  // dp array will have the possible max subarray sum at that current index, as the one element itself is a subarray of an array so at any position max subarray sum = Math.Max(nums[i], nums[i]+ dp[i -1]), current position item if is still maximum of subarraySum of nums[i]+dp[i-1](previous max which we have already calculated) 
+                  int[] dp = new int[nums.Length];
+                  dp[0] = nums[0];
+                  int maxSum = dp[0];
+                  for(int i = 1; i < nums.Length; i++){
+                    int currentMaxSum = nums[i] + dp[i -1];
+                    dp[i] = Math.Max(currentMaxSum, nums[i]);
+                    maxSum = Math.Max(maxSum, dp[i]);
+                  }
+
+                  return maxSum;
                 }
-                else
-                    currentMax += nums[i];
-
-                if (globalMax < currentMax)
-                    globalMax = currentMax;
             }
-
-            return globalMax;
         }
     }
 }
